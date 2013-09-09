a18d1988b06f0cb57454358c48f8a56e1c7247e8
imasho <imasho@gmail.com>
2013-09-09 11:44:59 +0900

    implement expiration seconds and dbid

--- test/kyototycoon/client/test_client.rb
+++ test/kyototycoon/client/test_client.rb
@@ -2,20 +2,17 @@ require "helper"
 
 class ClientTest < Test::Unit::TestCase
   def setup
-    @client = (ENV["HOST"] && ENV["PORT"]) ? Kyototycoon::Client.new(ENV["HOST"], ENV["PORT"].to_i) : nil
+    raise "host and port of KyotoTycoon test server is required: rake test HOST=xxx PORT=xxx" if (!ENV["HOST"] || !ENV["PORT"])
+    @client = Kyototycoon::Client.new(ENV["HOST"], ENV["PORT"].to_i) 
   end
 
   def test_open
-    return unless @client
-
     @client.open
     @client.close
   end
 
 
   def test_clud
-    return unless @client
-
     @client.open
     assert_equal(@client.set("key1", "value1"), true)
     assert_equal(@client.get("key1"), "value1")
@@ -26,8 +23,6 @@ class ClientTest < Test::Unit::TestCase
   end
 
   def test_clud_bulk
-    return unless @client
-
     @client.open
     assert_equal( @client.set_bulk( {"key1" => "valueA", "key2" => "valueB", "key3" => "valueC"} ), 3)
     assert_equal( @client.get_bulk( ["key1", "key2", "key3", "key4"] ), 
@@ -35,6 +30,27 @@ class ClientTest < Test::Unit::TestCase
     assert_equal( @client.remove_bulk( ["key1", "key2"] ), 2 )
     assert_equal( @client.get_bulk( ["key1", "key2", "key3", "key4"] ), 
                                     {"key3" => "valueC"} )
+    @client.close
+  end
+
+  def test_expire
+    @client.open
+    assert_equal(@client.set_bulk( {"key1" => ["value1", 1]} ), 1)   # expire after 1 second
+    assert_equal(@client.set_bulk( {"key2" => "value2"} ), 1)        # not expire
+    assert_equal(@client.get("key1"), "value1")
+    assert_equal(@client.get("key2"), "value2")
+    sleep 3
+    assert_equal(@client.get("key1"), nil)
+    assert_equal(@client.get("key2"), "value2")
+    @client.close
+  end
+
+  def test_dbid
+    @client.open
+    assert_equal(@client.set_bulk( {"key1" => "value1"}, 0 ), 1)
+    assert_equal(@client.set_bulk( {"key1" => "value1"}, 1 ), 0)
+    assert_equal(@client.get("key1", 0), "value1")
+    assert_equal(@client.get("key1", 1), nil)
     @client.close
   end
 
